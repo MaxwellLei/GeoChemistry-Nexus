eab97353d047833ad9fb2291e53d715a1bcf7943
LSeraphine <738232585@qq.com>
2025-06-16 09:21:58 +0800

    修改了底图模板列表树样式

--- src/Views/MainPlotPage.xaml.cs
+++ src/Views/MainPlotPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -392,6 +393,58 @@ namespace GeoChemistryNexus.Views
                 pointsY = null;
             }
             status = -1;
+        }
+
+        // 点击展开列表
+        private void OnTreeViewItemMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // 校验事件的发送者是否就是事件的原始来源所在的TreeViewItem。阻止冒泡事件
+            if (sender is TreeViewItem item)
+            {
+                // 从真正被点击的元素向上查找父类
+                var sourceTvi = FindAncestor<TreeViewItem>(e.OriginalSource as DependencyObject);
+
+                // 冒泡事件
+                if (item != sourceTvi)
+                {
+                    return;
+                }
+            }
+
+            // 检查父类状态
+            var originalSource = e.OriginalSource as DependencyObject;
+            if (FindAncestor<ToggleButton>(originalSource) != null)
+            {
+                return;
+            }
+
+            // 执行展开/折叠
+            if (sender is TreeViewItem tvi && tvi.HasItems)
+            {
+                tvi.IsExpanded = !tvi.IsExpanded;
+            }
+
+            // 停止事件冒泡
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// 辅助方法，用于在可视化树中向上查找指定类型的父控件。
+        /// </summary>
+        /// <typeparam name="T">要查找的父控件的类型。</typeparam>
+        /// <param name="current">查找的起始依赖对象。</param>
+        /// <returns>找到的第一个指定类型的父控件，如果找不到则返回 null。</returns>
+        private T FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        {
+            while (current != null)
+            {
+                if (current is T ancestor)
+                {
+                    return ancestor;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            }
+            return null;
         }
     }
 }
